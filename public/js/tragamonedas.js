2a861b0ec1581332261a34db5c5cd05802d30dc6
JeanPCode <136622089+jeanpii96@users.noreply.github.com>
2024-06-19 21:46:11 -0400

    ajuste tragamonedas

--- public/js/tragamonedas.js
+++ public/js/tragamonedas.js
@@ -2,7 +2,6 @@ let girar = false;
 let status = document.getElementById("status"); 
 let recharging = false;
 
-// Función para girar la máquina tragamonedas
 function maquina(){
 	if (girar || (score === 0 && !recharging)) {
 		status.innerHTML = "RECARGA! NO TIENES PUNTOS DE JUEGO";
@@ -23,7 +22,6 @@ function maquina(){
     slot2 = setInterval(spin2, 50);
     slot3 = setInterval(spin3, 50);
 
-    // Función para el giro del slot 1
 	function spin1(){
 		i1++;
 		if (i1>=numeberSlot1){
@@ -37,7 +35,6 @@ function maquina(){
 		slotTile.className = "a"+(parseInt(slotTile.className.substring(1))+1)
 	}
 
-    // Función para el giro del slot 2
 	function spin2(){
 		i2++;
 		if (i2>=numeberSlot2){
@@ -51,7 +48,6 @@ function maquina(){
 		slotTile.className = "a"+(parseInt(slotTile.className.substring(1))+1)
 	}
 
-    // Función para el giro del slot 3
 	function spin3(){
 		i3++;
 		if (i3>=numeberSlot3){
@@ -67,14 +63,11 @@ function maquina(){
 	}
 }
 
-
-// Función para verificar si el jugador ha ganado
 function pruebaGanador(){
 	let slot1 = document.getElementById("slot1").className
 	let slot2 = document.getElementById("slot2").className
 	let slot3 = document.getElementById("slot3").className
 
-    // condicional para determinar cuando se gana y se pierde
 	if (((slot1 == slot2 && slot2 == slot3) ||
 		(slot1 == slot2 && slot3 == "a9") ||
 		(slot1 == slot3 && slot2 == "a9") ||
@@ -95,14 +88,12 @@ function pruebaGanador(){
 	recharging = false; 
 }
 
-//Funcion para crear numero aleatorio
 function numeroAleatorio(min, max){
 	return Math.floor((Math.random() * (max-min+1)) + min);
 }
 
 let score = 0;
 
-//Funcion para recargar puntaje
 const recargar = document.getElementById("recarga");
 recargar.addEventListener("click", () => {
   score += 100; 
